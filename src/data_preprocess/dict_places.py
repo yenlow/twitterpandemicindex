f9d990d2cd971de2c27d14c267a615b568a9507f
yenlow <yenlow@gmail.com>
2020-04-20 21:44:53 -0700

    country/state abbrev to full names

---
+++ src/data_preprocess/dict_places.py
@@ -0,0 +1,92 @@
+# United States of America Python Dictionary to translate States,
+# Districts & Territories to Two-Letter codes and vice versa.
+#
+# https://gist.github.com/rogerallen/1583593
+#
+# Dedicated to the public domain.  To the extent possible under law,
+# Roger Allen has waived all copyright and related or neighboring
+# rights to this code.
+
+# Nonsensical places to exclude from search
+excluded_places = ['', "none",'\n', '\\n','europe','planet earth','everywhere',
+                'narnia','basement','closet','east coast!','in the 6',
+                   'hell','heaven','worldwide','global','world',
+                   'text resist to 50409','doctor gonzalez','africa',
+                   'he/she','she/her','internet']
+# ensure lowercase
+excluded_places = [i.lower() for i in excluded_places]
+
+# remap problematic names to known ones
+remap_dict = {
+'america' : 'us',
+'southern california': 'los angeles',
+'distrito federal, méxico': 'mexico city',
+'cdmx':'mexico city',
+'ny':'new york',
+'uk':'united kingdom',
+'guadalajara, jalisco': 'guadalajara, jalisco, mexico',
+'south east, england': 'england',
+'republic of the philippines':'philippines'
+}
+
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands':'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
+
+# thank you to @kinghelix and @trevormarburger for this idea
+abbrev_us_state = dict(map(reversed, us_state_abbrev.items()))
--- src/data_preprocess/normalize_places.py
+++ src/data_preprocess/normalize_places.py
@@ -4,6 +4,8 @@
 import pandas as pd
 from itertools import islice
 import geocoder
+from country_converter import CountryConverter
+from data_preprocess.dict_places import *
 from api.config import mapquest_api
 
 pd.set_option('display.max_columns', 100)
@@ -13,27 +15,6 @@ pd.set_option('display.max_rows', 300)
 unnormalized_loc_file = 'data/locations_test.tsv'
 loc_mappings_outfile = 'data/locations_mapping.tsv'
 mapquest_key = mapquest_api()
-
-# Nonsensical places to exclude from search
-excluded_places = ['', "none",'\n', '\\n','europe','planet earth','everywhere',
-                'narnia','basement','closet','east coast!','in the 6',
-                   'hell','heaven','worldwide','global','world',
-                   'text resist to 50409','doctor gonzalez','africa',
-                   'he/she','she/her','internet']
-# ensure lowercase
-excluded_places = [i.lower() for i in excluded_places]
-
-# remap problematic names to known ones
-remap_dict = {
-'america' : 'US',
-'southern California': 'los angeles',
-'distrito federal, méxico': 'mexico city',
-'cdmx':'mexico city',
-'ny':'New York',
-'guadalajara, jalisco': 'guadalajara, jalisco, mexico',
-'south east, england': 'england',
-'republic of the philippines':'philippines'
-}
 
 places_input = []
 places_output = []
@@ -61,19 +42,32 @@ with open(unnormalized_loc_file, newline='\n') as f:
                                         result.country, result.state,
                                         result.county, result.city])
 
-colnames = ['address','type','country_code','state','county','city']
+colnames = ['place_norm','type','country_code','state','county','city']
 place_df = pd.DataFrame(places_output, columns = colnames)
-place_df['original'] = places_input
-print(place_df.head())
+place_df['place_original'] = places_input
+print(place_df.sample(50))
 
-# bug in mapquest: replace 'US' in address with state abbrev if available
-m = (place_df.address=='US') & (place_df.state!='')
-place_df.address[m] = place_df.state[m]
-place_df[m]
+# Instead of inaccurate street names, use either citty or country
+m = (place_df.type=='STREET')
+place_df.place_norm[m] = place_df.city[m].combine_first(place_df.country_code[m])
 
-# TODO: recode country code in address to country name to avoid ambiguity with state abbrev
+# Fix bug in mapquest: replace 'US' with state if available
+m = (place_df.place_norm=='US') & (place_df.type=='STATE') & (place_df.state!='')
+place_df.place_norm[m] = place_df.state[m]
+
+# Recode country code to country name to avoid ambiguity with state abbrev
 # e.g. ID = Indonesia or Idaho
+cc = CountryConverter()
+m = (place_df.place_norm==place_df.country_code) & (place_df.type=='COUNTRY')
+place_df.place_norm[m] = place_df.place_norm[m].apply(cc.convert, to='name_short')
 
+# Recode state abbrev to state name to avoid ambiguity with country code
+# e.g. ID = Indonesia or Idaho
+m = (place_df.place_norm==place_df.state) & (place_df.type=='STATE')
+place_df.place_norm[m] = place_df.place_norm[m].apply(abbrev_us_state.get)
+print(place_df.sample(50))
+
+# Save final mapping table
 place_df.to_csv(loc_mappings_outfile, sep="\t", index=False)
 
 
