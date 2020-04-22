59855649667f0e497a812434d5c1fc7d8e3db0a0
yenlow <yenlow@gmail.com>
2020-04-22 03:52:39 -0700

    loc_mapping in pd

--- src/text_preprocess/normalize_places.py
+++ src/text_preprocess/normalize_places.py
@@ -3,20 +3,24 @@
 
 import pandas as pd
 from itertools import islice
-import geocoder
+import geocoder, re
 from country_converter import CountryConverter
 from text_preprocess.dict_places import *
 from api.config import mapquest_api
+from flag import dflagize
 
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_rows', 300)
 
-unnormalized_loc_file = '../../data/locations_test.tsv'
+unnormalized_loc_file = 'data/locations_test1.tsv'
 loc_mappings_outfile = 'data/locations_mapping.tsv'
 mapquest_key = mapquest_api()
 
+cc = CountryConverter()
+
 places_input = []
+places_queried = []
 places_output = []
 with open(unnormalized_loc_file, newline='\n') as f:
     next(f) #skip header line
@@ -27,12 +31,18 @@ with open(unnormalized_loc_file, newline='\n') as f:
         else:
             places_100 = []
             for i in next_n_lines:
-                place = i.split('\t')[0].strip().lower()
-                if place not in excluded_places:
+                place_ori = i.split('\t')[0]
+                place = place_ori.strip().lower()
+                if place in emoji_flags:
+                    place_queried = cc.convert(dflagize(place)[1:3], to='name_short')
+                elif not re.match(blacklist_regex, place) and place not in excluded_places:
                     if place in remap_dict:
-                        place = remap_dict[place]
-                    places_100.append(place)
-                    places_input.append(place)
+                        place_queried = remap_dict[place]
+                    else:
+                        place_queried = place
+                    places_100.append(place_queried)
+                    places_queried.append(place_queried)
+                    places_input.append(place_ori)
 
             # Call mapquest API to query place names (up to 100 per batch)
             g = geocoder.mapquest(places_100, method='batch', key=mapquest_key)
@@ -40,32 +50,38 @@ with open(unnormalized_loc_file, newline='\n') as f:
             for result in g:
                 places_output.append([result.address, result.quality,
                                         result.country, result.state,
-                                        result.county, result.city])
+                                        result.county, result.city,
+                                        result.lat, result.lng])
 
-colnames = ['place_norm','type','country_code','state','county','city']
+colnames = ['place_norm','type','country_code','state','county','city', 'latitude','longitude']
 place_df = pd.DataFrame(places_output, columns = colnames)
 place_df['place_original'] = places_input
+place_df['place_queried'] = places_queried
+col_order = ['place_original','place_queried'] + colnames
+place_df = place_df[col_order]
 print(place_df.sample(50))
 
 # Instead of inaccurate street names, use either citty or country
 m = (place_df.type=='STREET')
 place_df.place_norm[m] = place_df.city[m].combine_first(place_df.country_code[m])
 
-# Fix bug in mapquest: replace 'US' with state if available
-m = (place_df.place_norm=='US') & (place_df.type=='STATE') & (place_df.state!='')
+# Fix bug in mapquest: replace country_code with state if available
+m = (place_df.type=='STATE') & (place_df.state!='')
 place_df.place_norm[m] = place_df.state[m]
 
 # Recode country code to country name to avoid ambiguity with state abbrev
 # e.g. ID = Indonesia or Idaho
-cc = CountryConverter()
 m = (place_df.place_norm==place_df.country_code) & (place_df.type=='COUNTRY')
 place_df.place_norm[m] = place_df.place_norm[m].apply(cc.convert, to='name_short')
 
 # Recode state abbrev to state name to avoid ambiguity with country code
 # e.g. ID = Indonesia or Idaho
-m = (place_df.place_norm==place_df.state) & (place_df.type=='STATE')
+m = (place_df.country_code=='US') & (place_df.place_norm==place_df.state) & (place_df.type=='STATE')
 place_df.place_norm[m] = place_df.place_norm[m].apply(abbrev_us_state.get)
+#TODO: do the same for brazil, MX states
+
 print(place_df.sample(50))
+
 
 # Save final mapping table
 place_df.to_csv(loc_mappings_outfile, sep="\t", index=False)
